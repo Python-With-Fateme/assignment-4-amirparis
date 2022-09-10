e808f99513089e4b991de5222cd8612fcb10e6be
unknown <you@example.com>
2022-09-10 22:21:59 +0430

    first

--- as4-4.py
+++ as4-4.py
@@ -1,1 +1,24 @@
+import random
+words=['dualipa' , 'harrystyles' , 'imaginedragons' , 'theweeknd' , 'coldplay' , 'shawnmendes' , 'halsey' , 'billieeilish' , 'postmalone' , 'lanadelrey' , 'eminem' , 'arianagrande' , 'edsheeran' ]
+word=random.choice(words)
+word=list(word)
+true_chars=['-']*len(word)
+print(true_chars)
+score=len(word)
+print('Singers')
+while True :
+    char=input('enter a char :')
+    for i in range(len(word)) :
+        if word[i]==char :
+            true_chars[i]=char
+    if char not in word :
+        score-=1
+        print(score)
 
+    print(true_chars)
+    if word==true_chars :
+        print('you win :) ')
+        break
+    if score==0 :
+        print('you lost :( ')
+        break
